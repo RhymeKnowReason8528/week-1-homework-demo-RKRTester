9c38d6a0bc81b1cfb2d61211376ebd3bf1f4a4ce
NoahAndrews <noahandrews@users.noreply.github.com>
2017-03-11 11:19:28 -0500

    Completed Homework

--- src/main/java/com/rhymeknowreason/HelloWorld.java
+++ src/main/java/com/rhymeknowreason/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         // TODO; Print "Hello World!" (without the quotes) to the terminal here.
         System.out.println("Hello World!");
+
     }
 }
